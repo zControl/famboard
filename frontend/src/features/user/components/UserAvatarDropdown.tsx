66c792b8c8eaad24cb84c496d1bc60e848e6c0e7
ZS iMac <zsolis@deltacontrols.com>
2024-12-27 21:50:26 -0800

    Added navItems and logout

--- frontend/src/features/user/components/UserAvatarDropdown.tsx
+++ frontend/src/features/user/components/UserAvatarDropdown.tsx
@@ -10,10 +10,24 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { LogoutButton } from "@/features/auth/components/LogoutButton";
 import { UserAvatar } from "@/features/user/components/UserAvatar";
 import { useProfile } from "@/features/user/hooks/useProfile";
 import { useNavigate } from "@tanstack/react-router";
 import { IceCream2Icon } from "lucide-react";
+
+const navItems = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: <IceCream2Icon />,
+  },
+  {
+    label: "Profile",
+    href: "/profile",
+    icon: <IceCream2Icon />,
+  },
+];
 
 export const UserAvatarDropdown = () => {
   const navigate = useNavigate();
@@ -29,30 +43,41 @@ export const UserAvatarDropdown = () => {
             <div className="flex items-center space-x-4 ">
               <UserAvatar />
               <div className="flex flex-col">
-                <div>{profile?.email}</div>
                 <div>{profile?.username}</div>
+                <div>{profile?.group}</div>
               </div>
             </div>
           </SheetTitle>
           <SheetDescription>
-            <span>Status</span>
+            <span>Status (TODO)</span>
           </SheetDescription>
         </SheetHeader>
         <Separator className="my-4" />
-        <SheetClose asChild>
-          <Button
-            size="lg"
-            className="px-2 w-full flex items-center justify-start text-md text-muted-foreground"
-            variant={"ghost"}
-            onClick={() => navigate({ to: "/profile" })}
-          >
-            <IceCream2Icon />
-            <span>Your profile</span>
-          </Button>
-        </SheetClose>
+        {navItems.map((item) => (
+          <SheetClose asChild key={item.href}>
+            <Button
+              size="lg"
+              className="px-2 w-full flex items-center justify-start text-md text-muted-foreground"
+              variant={"ghost"}
+              onClick={() => {
+                const [path, hash] = item.href.split("#");
+                navigate({ to: path, hash: hash || undefined });
+              }}
+            >
+              {item.icon}
+              <span>{item.label}</span>
+            </Button>
+          </SheetClose>
+        ))}
+        <Separator className="my-4" />
+        OTHER THINGS
+        <Separator className="my-4" />
+        OTHER THINGS
+        <Separator className="my-4" />
+        OTHER THINGS
         <Separator className="my-4" />
         <SheetFooter>
-          <SheetClose>Close</SheetClose>
+          <LogoutButton />
         </SheetFooter>
       </SheetContent>
     </Sheet>
