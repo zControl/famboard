7a1b5ffc2a1f2607acfaac8d56aa0f8617a1ec74
ZS iMac <zsolis@deltacontrols.com>
2024-12-30 16:28:54 -0800

    fixed tasks by user endpoint return object.

--- frontend/src/features/parents/components/KidSummaryTile.tsx
+++ frontend/src/features/parents/components/KidSummaryTile.tsx
@@ -6,7 +6,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
-import { KidProfileSummaryCard } from "@/features/parents/components/KidProfileSummaryCard";
 import { useKidManager } from "@/features/parents/hooks/useKidManager";
 
 interface KidSummaryTileProps {
@@ -29,31 +28,35 @@ export const KidSummaryTile = ({ id }: KidSummaryTileProps) => {
   };
 
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex flex-row justify-between items-center">
-          <div>
-            <CardTitle className="text-xl">
-              {kid?.firstName || "No Name!"}
-            </CardTitle>
+    <>
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row justify-between items-center">
+            <div>
+              <CardTitle className="text-xl">
+                {kid?.firstName || "No Name!"}
+              </CardTitle>
+            </div>
+            <div>actions</div>
           </div>
-          <div>actions</div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <div className="grid grid-cols-1 md:flex md:flex-row justify-center">
-          <div className="w-1/4 mx-auto p-4">
-            {kid && <KidProfileSummaryCard kid={kid} />}
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4">
+            <div>
+              <KidProfileSummary id={id} />
+            </div>
+            <div>
+              <KidActiveTasks />
+            </div>
+            <div>
+              <KidNeedsApproval />
+            </div>
           </div>
-          <div className="flex flex-row justify-around w-full gap-x-4">
-            {/* <KidActiveTasksCard userId={kid?.userId ?? ""} /> */}
-            <NeedsApprovalCard />
-          </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="w-full mx-auto text-center">{kid?.userId}</div>
-      </CardFooter>
-    </Card>
+        </CardContent>
+        <CardFooter>
+          <div className="w-full mx-auto text-center">{kid?.userId}</div>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
