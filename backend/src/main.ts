6cff98a7b08388666eb88017b4e67debe6d181d8
ZS iMac <zsolis@deltacontrols.com>
2025-09-09 23:06:30 -0700

    Ensure environment is loaded at the application entry point.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,10 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
