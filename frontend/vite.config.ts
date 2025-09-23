1894a59e1ed4040b1752553c2cc765fab6daefae
ZS iMac <zsolis@deltacontrols.com>
2025-09-22 22:43:17 -0700

    Update tanstack router plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
 import path from "path"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
