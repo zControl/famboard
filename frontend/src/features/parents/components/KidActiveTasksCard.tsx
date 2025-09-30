4b427cd7b1b891c9ee24026034056b46c1d9b6fb
ZS iMac <zsolis@deltacontrols.com>
2025-09-29 22:18:08 -0700

    Add sequence number and improve column titles.

--- frontend/src/features/parents/components/KidActiveTasksCard.tsx
+++ frontend/src/features/parents/components/KidActiveTasksCard.tsx
@@ -1,24 +1,29 @@
 import { DataTableCore } from "@/components/datatable/DataTableCore";
+import { ColumnTitle } from "@/components/datatable/headers/ColumnTitle";
+import { CustomLink } from "@/components/ui/custom-link";
 import { Spinner } from "@/components/ui/spinner";
 import { useUserAssignedTasks } from "@/features/tasks/hooks/useUserAssignedTasks";
 import { ColumnDef } from "@tanstack/react-table";
-
-interface CustomHeaderProps {
-  title: string;
-  className?: string;
-}
-
-export const CustomHeader = ({ title, className }: CustomHeaderProps) => (
-  <div className={className}>{title}</div>
-);
 
 const activeTasksColumns: ColumnDef<{
   sequenceNumber: number;
   title: string;
 }>[] = [
   {
+    accessorKey: "number",
+    header: () => <ColumnTitle title="ID" />,
+    cell: ({ row }) => (
+      <CustomLink
+        to={`/parents/tasks/${row.original.sequenceNumber}`}
+        size="lg"
+      >
+        {row.original.sequenceNumber}
+      </CustomLink>
+    ),
+  },
+  {
     accessorKey: "title",
-    header: () => <CustomHeader title="Title" />,
+    header: () => <ColumnTitle title="Title" />,
     cell: ({ row }) => <div>{row.original.title}</div>,
   },
 ];
@@ -26,7 +31,7 @@ const activeTasksColumns: ColumnDef<{
 export const KidActiveTasksCard = ({ userId }: { userId: string }) => {
   const { assignedTasks, isLoading } = useUserAssignedTasks(userId);
   return (
-    <div className="w-full border rounded-lg px-2">
+    <div className="w-full border rounded-lg">
       {isLoading ? (
         <Spinner />
       ) : (
